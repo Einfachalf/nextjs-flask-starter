f6ffa5a9bc4063063770e9ec07bb74c4db705b9d
EinfachAlex110 <150645528+AILEXGPT@users.noreply.github.com>
2024-01-28 11:38:36 +0100

    nvnv

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
           >
             By{' '}
             <Image
-              src="/vercel.svg"
+              src="/ealogo.png"
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
